8ed04f66657e20ca0b0826eaf675b79e9948b884
diksharajput-raj <maltidevi77114@gmail.com>
2025-08-20 20:43:51 +0530

    Add files via upload

---
+++ code 2.py
@@ -0,0 +1,9 @@
+# Take input from user
+num = int(input("Enter a number: "))
+
+# Check if last digit is 4
+if num % 10 == 4:
+    print("The last digit is 4.")
+else:
+    print("The last digit is not 4.")
+    
